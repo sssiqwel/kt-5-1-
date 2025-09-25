c51a6bc6a6bf9ba35a679878c298ae16f3ed2b39
User <User@DESKTOP-75A86ME>
2025-09-25 19:59:25 +0300

    Добавьте файлы проекта.

---
+++ kt 5/Program.cs
@@ -0,0 +1,51 @@
+﻿public interface IAnimal
+{
+    string Name { get; set; }
+    void MakeSound();
+}
+
+public class Dog : IAnimal
+{
+    public string Name { get; set; }
+
+    public Dog(string name)
+    {
+        Name = name;
+    }
+
+    public void MakeSound()
+    {
+        Console.WriteLine($"{Name} говорит: Гав-гав!");
+    }
+}
+public class Cat : IAnimal
+{
+    public string Name { get; set; }
+
+    public Cat(string name)
+    {
+        Name = name;
+    }
+
+    public void MakeSound()
+    {
+        Console.WriteLine($"{Name} говорит: Мяу-мяу!");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        IAnimal dog = new Dog("Бобик");
+        IAnimal cat = new Cat("Мурка");
+        dog.MakeSound(); 
+        cat.MakeSound(); 
+        IAnimal[] animals = new IAnimal[] { dog, cat };
+
+        Console.WriteLine("\nВсе животные издают звуки:");
+        foreach (var animal in animals)
+        {
+            animal.MakeSound();
+        }
+    }
+}
